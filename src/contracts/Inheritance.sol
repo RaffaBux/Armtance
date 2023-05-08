38af218faef7acaf30029030146e753e12ace8c5
Raffa Bux <raffaelebussolotto@gmail.com>
2023-05-08 12:12:59 +0200

    updated contracts structure

--- src/contracts/Inheritance.sol
+++ src/contracts/Inheritance.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.19 <0.9.0;
 
 import './SelfSovereignIdentity.sol';
-import './Settings.sol';
 
-struct Degree {
-	string degreeType;
-	string name;
-}
+// struct Degree {
+// 	string degreeType;
+// 	string name;
+// }
 
 struct CredentialSubject {
 	string id;
-	Degree degree;
+	// Degree degree;
 }
 
 struct Proof {
@@ -32,43 +31,84 @@ struct VerifiableCredential {
 	Proof proof;
 }
 
-
-struct Issuer {
+struct Issuer {	// ?? -> non mi ricordo di preciso chi è ç-ç
 	address issuerAddress;
 	string issuerDid;
+	string signature;
+}
+
+struct Heir {
+	uint id;
+	address payable heirAddress;
+	bool delegated;
+	uint amount;
 }
 
 contract Inheritance {
 
 	VerifiableCredential private vC;
-	SelfSovereignIdentity private ssi;
-	string private deceasedDid;
+	Issuer private inheritanceOwner;
 
-	Issuer private trustedIssuer;
-	mapping(string => Heir) private heirs;
+	SelfSovereignIdentity private ssi; // test line
+	
+	mapping(uint => string) private heirsIdToDids;
+	mapping(string => Heir) private heirsDidToHeir;
+	uint numberOfHeirs;
 
 	constructor(
-		VerifiableCredential memory _verifiableCredential,
-		Heir[] memory heirList,
-		string memory 
-	) {
+		string[] memory _heirsDid,
+		address payable[] memory _addresses, 
+		bool[] memory _delegations, 
+		uint[] memory _amounts,
+		string memory ownerDid,
+		string memory _signature
+	) {	
 
-		vC = _verifiableCredential;
-		ssi = new SelfSovereignIdentity();
+		ssi = new SelfSovereignIdentity(); // test line
 
-		string memory trustedIssuerDid = ssi.createDid(); //ssi.createDid().(msg.sender) == this.(msg.sender) ?
-		trustedIssuer = Issuer(msg.sender, trustedIssuerDid);
-
-		for(uint i = 0; i < heirList.length; i++) {
-			address tmpHeirAddress = heirList[i].heirAddress;
-
-			string memory heirTrustedDid = ssi.createChildTrustedDid(tmpHeirAddress, trustedIssuerDid);
-
-			heirs.push(
-				Heir(tmpHeirAddress, heirTrustedDid, trustedIssuerDid)
-			);
-		}
-
+		inheritanceOwner = Issuer(msg.sender, ownerDid, _signature);
+		settings(_heirsDid, _addresses, _delegations, _amounts);
 	}
 
+	// → posso già ricevere un oggetto Heir come param o è più efficiente se
+	// 		me lo costruisco qua?
+	function settings(
+		string[] memory _dids,
+		address payable[] memory _addresses, 
+		bool[] memory _delegations, 
+		uint[] memory _amounts
+	) private {
+		numberOfHeirs = 0;
+		for(uint i = 0; i < _dids.length; i++) {
+			heirsIdToDids[i] = _dids[i];
+			heirsDidToHeir[_dids[i]] = Heir(i, _addresses[i], _delegations[i], _amounts[i]);
+			numberOfHeirs++;
+		}
+	}
+
+	// → la VC mi arriva come stringa? non mi ricordo...
+	//		nel caso dovrei riformattarla
+	function verify(VerifiableCredential memory _vC) public {
+		vC = _vC;
+		bool verified = false;
+		if(verified) {
+			split();
+		} else {
+			split();
+		}		
+	}
+
+	function split() private {
+		uint idAmount;
+		address payable idAddress;
+		
+		for(uint i = 0; i < numberOfHeirs; i++) {
+			idAmount = heirsDidToHeir[heirsIdToDids[i]].amount;
+			idAddress = heirsDidToHeir[heirsIdToDids[i]].heirAddress;
+			
+			// si prende i soldi direttamente dal conto dell'issuer?
+			(bool success, bytes memory data) = idAddress.call{value: idAmount}(""); // reentrancy?
+			require(success, 'Fato caca adoso :C');
+		}
+	}
 }
