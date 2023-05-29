166a5e0f8360a7b6762057c33d473fe0ab544fea
Raffa Bux <raffaelebussolotto@gmail.com>
2023-05-29 07:23:03 +0200

    up to date commit

--- src/components/Split/Split.js
+++ src/components/Split/Split.js
@@ -4,8 +4,28 @@ import React from 'react';
 export default function Split() {
   
   return(
-    <div>
-      <h1>TEST</h1>
+    <div className='split'>
+      <div className='centredRowContainer announcement'>
+        <h3>
+          ciao ₍´｡• ◡ •｡`₎ sono Gilberto, erede legittimo di Armando 
+          ₍ ︶︿︶₎ e mi sono autenticato come il mio caro, ehm..., 
+          z-zietto (??) ૮₍˶Ó﹏Ò ₎ა fornendo questa Verifiable Credential 
+          TOTALMENTE autentica ₍づ◡﹏◡₎づ
+        </h3>
+      </div>
+      <hr/>
+      <div className='centredRowContainer'>
+        <div>
+            <div className='buttonContainer' onClick={ () => { alert('carica vc :3') }}>
+              <h5 className='buttonText'>CARICA VERIFIABLE CREDENTIAL</h5>
+            </div>
+        </div>
+        <div>
+          <div className='buttonContainer' onClick={ () => { alert('controllo vc ed eventualmente smisto eredità') }}>
+            <h5 className='buttonText'>SMISTA EREDITA'</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
